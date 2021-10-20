b52f1ba4e5e00594f6588c6d13ac588e239cd3d9
mkhoa0201 <mkhoa0201@gmail.com>
2021-10-20 11:54:37 +0300

    Dice 2 random

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
+var randomNum2 = Math.floor(Math.random() * 6) + 1; //random Number 1-6
+
+var randomImageSource2 = "images/dice" + randomNum2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
 
