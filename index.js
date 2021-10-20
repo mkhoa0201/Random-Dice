92868fe53c7b4a15a2a06a9dc8b8616d73f20a9d
mkhoa0201 <mkhoa0201@gmail.com>
2021-10-20 13:01:04 +0300

    change color, add button

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randomNum1 = Math.floor(Math.random() * 6) + 1; //random Number 1-6
+let randomNum1 = Math.floor(Math.random() * 6) + 1; //random Number 1-6
 
 var randomDiceImage = "dice" + randomNum1 + ".png";
 
@@ -8,11 +8,30 @@ var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-var randomNum2 = Math.floor(Math.random() * 6) + 1; //random Number 1-6
+let randomNum2 = Math.floor(Math.random() * 6) + 1; //random Number 1-6
 
 var randomImageSource2 = "images/dice" + randomNum2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  }
+  else if (randomNum2 > randomNum1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+  function refreshPage(){
+    window.location.reload();
+} 
+  
+
+
+
+
 
 
 
